0778ad3f9cd78ff2c7e8c8d23d5b146bc419d3c7
James Orson <jamesaorson@gmail.com>
2022-01-07 23:53:14 -0800

    Get project compiling on Mac

---
+++ src/include/generation/c.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <parser/prelude.h>
+#include <macros/helpers.h>
+
+void generate_c_code(struct ProgramNode*);
+
+void generate_c_code(struct ProgramNode* program) {
+	UNUSED(program);
+}
