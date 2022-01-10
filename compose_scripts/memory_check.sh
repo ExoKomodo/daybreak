6f40a6bdcc0ad0399fb487cf8b7133ebdd079d16
James Orson <jorson@purestorage.com>
2022-01-10 11:16:58 -0800

    Update with new template repo defaults

--- compose_scripts/memory_check.sh
+++ compose_scripts/memory_check.sh
@@ -4,14 +4,4 @@ set -ex
 
 cd /app
 
-bash ./compose_scripts/build.sh daybreak
-
-VALGRIND_ARGS="-s --leak-check=full --show-leak-kinds=all --error-exitcode=42"
-
-# Directly compile programs using the compiler executable
-valgrind ${VALGRIND_ARGS} ./bin/daybreak ./examples/hello/hello.day
-
-bash ./compose_scripts/build.sh daybreak_test
-
-# Run tests with the compiler's test executable
-valgrind ${VALGRIND_ARGS} ./bin/daybreak_test
+bash ./compose_scripts/test.sh valgrind
