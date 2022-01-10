1a728702a412488a9388bae9dd6be1d6c78e4a2f
James Orson <jamesaorson@gmail.com>
2022-01-10 11:16:58 -0800

    Update with new template repo defaults

--- compose_scripts/test.sh
+++ compose_scripts/test.sh
@@ -2,14 +2,21 @@
 
 set -ex
 
+WRAPPER=""
+
+if [ $# -gt 0 ]; then
+	WRAPPER="$1"
+	echo "Wrapping test commands with: ${WRAPPER}"
+fi
+
 cd /app
 
 bash ./compose_scripts/build.sh daybreak
 
 # Directly compile programs using the compiler executable
-./bin/daybreak ./examples/hello/hello.day
+${WRAPPER} ./bin/daybreak ./examples/hello/hello.day
 
 bash ./compose_scripts/build.sh daybreak_test
 
 # Run tests with the compiler's test executable
-./bin/daybreak_test
+${WRAPPER} ./bin/daybreak_test
