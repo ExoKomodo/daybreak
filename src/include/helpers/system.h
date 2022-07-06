bbae9fc99ae7fef7544e8bea3e28d129222aec72
James Orson <jamesaorson@gmail.com>
2022-07-06 12:20:43 -0700

    Update system.h

--- src/include/helpers/system.h
+++ src/include/helpers/system.h
@@ -2,6 +2,7 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 
 #include <log/prelude.h>
@@ -19,12 +20,14 @@ int setup_language_directories();
 int _create_subdir(const char*, const char*);
 
 #if defined(_WIN32) || defined(_WIN64)
-  #define STANDARD_DIRECTORY "C:\\Program Files\\daybreak"
+  #define STANDARD_PATH_PREFIX "C:\\"
+  #define STANDARD_DIRECTORY "Program Files\\daybreak"
   #define OUTPUT_DIRECTORY "\\out\\"
   #define PACKAGE_DIRECTORY "\\packages\\"
   #define DEFAULT_EXECUTABLE "a.exe"
 #else
-  #define STANDARD_DIRECTORY "~/daybreak"
+  #define STANDARD_PATH_PREFIX getenv("HOME")
+  #define STANDARD_DIRECTORY "/daybreak"
   #define OUTPUT_DIRECTORY "/out/"
   #define PACKAGE_DIRECTORY "/packages/"
   #define DEFAULT_EXECUTABLE "a.out"
@@ -33,11 +36,15 @@ int _create_subdir(const char*, const char*);
 #define DEFAULT_SOURCE_OUTPUT "main.day.c"
 
 inline const char* get_standard_library_directory() {
-  const char* standard_directory_path = getenv("DAYBREAK_HOME");
-	if (!standard_directory_path) {
-    return STANDARD_DIRECTORY;
+  const char* daybreak_home_env = getenv("DAYBREAK_HOME");
+  if (daybreak_home_env) {
+    char* full_path = malloc(sizeof(char) * (strlen(daybreak_home_env) + 1));
+    strcpy(full_path, daybreak_home_env);
+    return full_path;
   }
-  return standard_directory_path;
+  char* full_path = malloc(sizeof(char) * (strlen(STANDARD_PATH_PREFIX) + strlen(STANDARD_DIRECTORY) + 2));
+  sprintf(full_path, "%s/%s", STANDARD_PATH_PREFIX, STANDARD_DIRECTORY);
+  return full_path;
 }
 
 int run_command(const char* command, FILE* output_stream) {
@@ -77,34 +84,40 @@ int _create_subdir(const char* directory, const char* subdirectory) {
 }
 
 char* get_default_output_file() {
-  const char* standard_library_directory = get_standard_library_directory();
+  char* standard_library_directory = get_standard_library_directory();
   char* output_file_path = malloc(sizeof(char) * (strlen(standard_library_directory) + strlen(OUTPUT_DIRECTORY) + strlen(DEFAULT_EXECUTABLE) + 1));
   sprintf(output_file_path, "%s" OUTPUT_DIRECTORY DEFAULT_EXECUTABLE, standard_library_directory);
+  free(standard_library_directory);
   return output_file_path;
 }
 
 int setup_language_directories() {
-  const char* standard_library_directory = get_standard_library_directory();
+  char* standard_library_directory = get_standard_library_directory();
   
   int error = _create_dir(standard_library_directory);
   if (error != 0) {
+    free(standard_library_directory);
     return errno;
   }
 
   error = _create_subdir(standard_library_directory, "bin");
   if (error != 0) {
+    free(standard_library_directory);
     return error;
   }
 
   error = _create_subdir(standard_library_directory, "packages");
   if (error != 0) {
+    free(standard_library_directory);
     return error;
   }
 
   error = _create_subdir(standard_library_directory, "out");
   if (error != 0) {
+    free(standard_library_directory);
     return error;
   }
 
+  free(standard_library_directory);
   return 0;
 }
