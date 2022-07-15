9d087ff180b3282cf3333a418424919f2686952d
James Orson <jamesaorson@gmail.com>
2022-07-14 18:46:52 -0700

    Update check_env.sh

--- compose_scripts/check_env.sh
+++ compose_scripts/check_env.sh
@@ -2,26 +2,31 @@
 
 set -ex
 
-if [ -z ${DAYBREAK_BOOTSTRAP} ]; then
+if [ -z ${DAYBREAK_BOOTSTRAP} ];
+then
 	echo "Must define env var: DAYBREAK_BOOTSTRAP"
 	exit 1
 fi
 
-if [ -z ${DAYBREAK_OUT} ]; then
+if [ -z ${DAYBREAK_OUT} ];
+then
 	echo "Must define env var: DAYBREAK_OUT"
 	exit 1
 fi
 
-if [ -z ${JENKINS_USER} ]; then
+if [ -z ${JENKINS_USER} ];
+then
 	echo "Must define env var: JENKINS_USER"
 	exit 1
 fi
 
-if [ -z ${JENKINS_GROUP} ]; then
+if [ -z ${JENKINS_GROUP} ];
+then
 	echo "Must define env var: JENKINS_GROUP"
 	exit 1
 fi
 
-if [ -n ${CC} ]; then
-	update-alternatives --set cc $(which ${CC})
+if [ -n ${CC_COMPILER} ];
+then
+	update-alternatives --set cc $(which ${CC_COMPILER})
 fi
