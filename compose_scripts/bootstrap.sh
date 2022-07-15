9375de956f308de9d8428ad77e8322c902f4ab46
James Orson <jamesaorson@gmail.com>
2022-07-14 18:46:04 -0700

    Update bootstrap.sh

--- compose_scripts/bootstrap.sh
+++ compose_scripts/bootstrap.sh
@@ -2,15 +2,15 @@
 
 set -ex
 
-TEMP=${CC}
+TEMP=${CC_COMPILER}
 # WASM does not support popen() currently, meaning daybreak is outside of the current WASM standard
-if [ ${CC} = "zig-wasi" ]; then
-	CC=zig
+if [ ${CC_COMPILER} = "zig-wasi" ]; then
+	CC_COMPILER=zig
 fi
 
 bash ./compose_scripts/check_env.sh
 
 ${DAYBREAK_BOOTSTRAP} ./src/main.day -o ${DAYBREAK_BOOTSTRAP}
 
-CC=${TEMP}
+CC_COMPILER=${TEMP}
 bash ./compose_scripts/check_env.sh
