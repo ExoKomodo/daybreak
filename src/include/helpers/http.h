d25694faa7d78361f6696a05d55d19b4d700a112
James Orson <jamesaorson@gmail.com>
2022-07-22 23:57:24 +0000

    Start http helpers

---
+++ src/include/helpers/http.h
@@ -0,0 +1,9 @@
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+char* get_ipv4_from_host(const char*);
+char* get_ipv4_from_host(const char* host) {
+	// Reference https://stackoverflow.com/q/52727565
+	return NULL;
+}
