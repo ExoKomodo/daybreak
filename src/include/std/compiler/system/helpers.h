99cb5058ce6ced4f2779184c4706187e8343a9b7
James Orson <jamesaorson@gmail.com>
2022-08-25 18:31:14 +0000

    Checkpoint

--- src/include/std/compiler/system/helpers.h
+++ src/include/std/compiler/system/helpers.h
@@ -11,7 +11,6 @@
 #endif
 
 char* system_get_default_output_file();
-char* system_get_standard_library_directory();
 int system_run_command(const char*, FILE*);
 int system_setup_language_directories();
 
@@ -32,18 +31,6 @@ int _system_make_subdir(const char*, const char*);
 #endif
 
 #define SYSTEM_DEFAULT_SOURCE_OUTPUT "main.day.c"
-
-inline char* system_get_standard_library_directory() {
-  const char* daybreak_home_env = getenv("DAYBREAK_HOME");
-  if (daybreak_home_env) {
-    char* full_path = malloc(sizeof(char) * (strlen(daybreak_home_env) + 1));
-    strcpy(full_path, daybreak_home_env);
-    return full_path;
-  }
-  char* full_path = malloc(sizeof(char) * (strlen(SYSTEM_STANDARD_PATH_PREFIX) + strlen(SYSTEM_STANDARD_DIRECTORY) + 2));
-  sprintf(full_path, "%s%s", SYSTEM_STANDARD_PATH_PREFIX, SYSTEM_STANDARD_DIRECTORY);
-  return full_path;
-}
 
 int system_run_command(const char* command, FILE* output_stream) {
   FILE* command_stream = popen(command, "r");
@@ -78,44 +65,5 @@ int _system_make_subdir(const char* directory, const char* subdirectory) {
     return error;
   }
   
-  return 0;
-}
-
-char* system_get_default_output_file() {
-  char* standard_library_directory = system_get_standard_library_directory();
-  char* output_file_path = malloc(sizeof(char) * (strlen(standard_library_directory) + strlen(SYSTEM_OUTPUT_DIRECTORY) + strlen(SYSTEM_DEFAULT_EXECUTABLE) + 1));
-  sprintf(output_file_path, "%s" SYSTEM_OUTPUT_DIRECTORY SYSTEM_DEFAULT_EXECUTABLE, standard_library_directory);
-  free(standard_library_directory);
-  return output_file_path;
-}
-
-int system_setup_language_directories() {
-  char* standard_library_directory = system_get_standard_library_directory();
-  
-  int error = _system_make_dir(standard_library_directory);
-  if (error != 0) {
-    free(standard_library_directory);
-    return errno;
-  }
-
-  error = _system_make_subdir(standard_library_directory, "bin");
-  if (error != 0) {
-    free(standard_library_directory);
-    return error;
-  }
-
-  error = _system_make_subdir(standard_library_directory, "packages");
-  if (error != 0) {
-    free(standard_library_directory);
-    return error;
-  }
-
-  error = _system_make_subdir(standard_library_directory, "out");
-  if (error != 0) {
-    free(standard_library_directory);
-    return error;
-  }
-
-  free(standard_library_directory);
   return 0;
 }
