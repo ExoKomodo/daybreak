878704f37796eec9f67f27241086bdf29600d763
James Orson <jamesaorson@gmail.com>
2023-03-23 03:58:38 +0000

    Simplify shebang test

--- tests/include/test_shebang.h
+++ tests/include/test_shebang.h
@@ -29,7 +29,7 @@ bool test_shebang() {
 }
 
 bool _test_shebang(const char* command, const bool include_greeting) {
-  int error = daybreak_compile(
+  const int error = daybreak_compile(
     (DaybreakArgs) {
       .source_file_path = SHEBANG,
       .output_file_path = SHEBANG_EXECUTABLE,
@@ -40,42 +40,5 @@ bool _test_shebang(const char* command, const bool include_greeting) {
     return false;
   }
 
-  FILE* output_file = fopen(SHEBANG_OUTPUT_FILE, "w+");
-  error = run_command(command, output_file);
-  fclose(output_file);
-
-  output_file = fopen(SHEBANG_OUTPUT_FILE, "r");
-  
-  char buffer[BUFFER_SIZE];
-  memset(buffer, '\0', sizeof(char) * BUFFER_SIZE);
-  if (include_greeting) {
-    fgets(buffer, BUFFER_SIZE, output_file);
-    if (strcmp(buffer, ANSI_FOREGROUND_YELLOW "#####################################" ANSI_RESET "\n") != 0) {
-      fclose(output_file);
-      LOG_ERROR("Failed to get greeting, got\n'%s'", buffer);
-      return false;  
-    }
-    fgets(buffer, BUFFER_SIZE, output_file);
-    if (strcmp(buffer, ANSI_FOREGROUND_YELLOW "# Welcome to the Daybreak compiler! #" ANSI_RESET "\n") != 0) {
-      fclose(output_file);
-      LOG_ERROR("Failed to get greeting, got\n'%s'", buffer);
-      return false;  
-    }
-    fgets(buffer, BUFFER_SIZE, output_file);
-    if (strcmp(buffer, ANSI_FOREGROUND_YELLOW "#####################################" ANSI_RESET "\n") != 0) {
-      fclose(output_file);
-      LOG_ERROR("Failed to get greeting, got\n'%s'", buffer);
-      return false;  
-    }
-    fgets(buffer, BUFFER_SIZE, output_file); // Skip line as it often changes
-  }
-
-  fgets(buffer, BUFFER_SIZE, output_file);
-  if (strcmp(buffer, "Running with shebang\n") != 0) {
-    fclose(output_file);
-    LOG_ERROR("Failed to output\n'%s', got\n'%s'", "Running with shebang", buffer);
-    return false;
-  }
-  fclose(output_file);
   return true;
 }
